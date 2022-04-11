039c42da0294a9ffd4b5cc615166a27e2cdd7800
Learning-Space-Practice <access.holoborodko.volodymyr@gmail.com>
2022-04-11 20:52:42 +0300

    form-edit

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ const registrationBtn = document.querySelector('.registration-btn')
 const box = document.querySelector('.enter')
 const box2 = document.querySelector('.enter-registration')
 
-enterBtn.addEventListener('click', () => {
-    box.classList.add('active')
-    box2.classList.remove('active')
-    console.log('click')
+enterBtn.addEventListener('click', () => {        
+        box.style = 'justify-content: flex-end'
+
 })
 
 registrationBtn.addEventListener('click', () => {
-    box2.classList.add('active')
-    box.classList.remove('active')
-    console.log('click')
+    box.style = 'justify-content: flex-start'
+
 })
